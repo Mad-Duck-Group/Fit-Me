cc5db2e6997cc625e302c841a579a0e5f9785520
MaFIaTH <mafeeyawat@gmail.com>
2025-07-24 15:42:59 +0700

    Scrolling Texture

---
+++ Assets/MadDuck/Scripts/UIs/Others/TestBounds.cs
@@ -0,0 +1,21 @@
+﻿using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace MadDuck.Scripts.UIs.Others
+{
+    public class TestBounds : MonoBehaviour
+    {
+        [Button("Debug Bounds")]
+        private void DebugBounds()
+        {
+            var renderer = GetComponent<Renderer>();
+            if (renderer == null)
+            {
+                Debug.LogError("SpriteRenderer component not found on this GameObject.");
+                return;
+            }
+            Bounds bounds = renderer.bounds;
+            Debug.Log($"Bounds Center: {bounds.center}, Size: {bounds.size}, Extents: {bounds.extents}");
+        }
+    }
+}
