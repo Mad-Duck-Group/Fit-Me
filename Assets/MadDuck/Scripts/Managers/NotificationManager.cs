2b11c23bda1f9cbe1db99acbb2bd5b4285348a80
MaFIaTH <mafeeyawat@gmail.com>
2025-08-20 13:03:13 +0700

    Notification System

---
+++ Assets/MadDuck/Scripts/Managers/NotificationManager.cs
@@ -0,0 +1,127 @@
+using System;
+using System.Collections.Generic;
+using Cysharp.Threading.Tasks;
+using MadDuck.Scripts.UIs.Notifications;
+using MessagePipe;
+using Sherbert.Framework.Generic;
+using Sirenix.OdinInspector;
+using Sirenix.Serialization;
+using UnityCommunity.UnitySingleton;
+using UnityEngine;
+
+namespace MadDuck.Scripts.Managers
+{
+    public struct NotificationDisplayEvent
+    {
+        public readonly NotificationType notificationType;
+        public readonly string message;
+        public readonly Sprite icon;
+
+        public NotificationDisplayEvent(NotificationType notificationType, string message, Sprite icon)
+        {
+            this.notificationType = notificationType;
+            this.message = message;
+            this.icon = icon;
+        }
+    }
+
+    public enum NotificationType
+    {
+        General,
+        Challenge
+    }
+
+    [Serializable]
+    public struct NotificationPrefabData
+    {
+        public NotificationView notificationViewPrefab;
+        public Vector2 initialPosition;
+    }
+    
+    [ShowOdinSerializedPropertiesInInspector]
+    public class NotificationManager : PersistentMonoSingleton<NotificationManager>, ISerializationCallbackReceiver, ISupportsPrefabSerialization
+    {
+        [Title("References")]
+        [OdinSerialize] private SerializableDictionary<NotificationType, NotificationPrefabData> notificationPrefabDictionary = new();
+        
+        [Title("Settings")]
+        [SerializeField] private float notificationStayDuration = 2f;
+        
+        [Title("Debug")]
+        [Button("Test Notification")]
+        private void TestNotification(NotificationDisplayEvent eventData)
+        {
+            EnqueueNotification(eventData);
+        }
+        
+        private readonly Queue<NotificationDisplayEvent> _notificationQueue = new();
+        private bool _showingNotification;
+        private IDisposable _notificationSubscription;
+
+        private void OnEnable()
+        {
+            _notificationSubscription = GlobalMessagePipe.GetSubscriber<NotificationDisplayEvent>().Subscribe(EnqueueNotification);
+        }
+        
+        private void OnDisable()
+        {
+            _notificationSubscription?.Dispose();
+        }
+        
+        private void EnqueueNotification(NotificationDisplayEvent eventData)
+        {
+            _notificationQueue.Enqueue(eventData);
+            if (!_showingNotification)
+            {
+                ShowNextNotification().Forget();
+            }
+        }
+
+        private async UniTaskVoid ShowNextNotification()
+        {
+            _showingNotification = true;
+            var notificationEvent = _notificationQueue.Dequeue();
+            if (!notificationPrefabDictionary.TryGetValue(notificationEvent.notificationType, out var prefabData))
+            {
+                Debug.LogWarning($"No notification prefab found for type: {notificationEvent.notificationType}");
+                _showingNotification = false;
+                return;
+            }
+            var view = Instantiate(prefabData.notificationViewPrefab, transform);
+            ((RectTransform)view.transform).anchoredPosition = prefabData.initialPosition;
+            view.Initialize();
+            view.SetMessage(notificationEvent.message);
+            view.SetIcon(notificationEvent.icon);
+            await view.Show();
+            await UniTask.WhenAll(UniTask.WaitForSeconds(notificationStayDuration),
+                view.PlayAnimation());
+            await view.Hide();
+            Destroy(view.gameObject);
+            _showingNotification = false;
+            if (_notificationQueue.Count > 0)
+            {
+                ShowNextNotification().Forget();
+            }
+        }
+        
+        #region Serialization
+        public void OnBeforeSerialize()
+        {
+            UnitySerializationUtility.SerializeUnityObject(this, ref serializationData);
+        }
+
+        public void OnAfterDeserialize()
+        {
+            UnitySerializationUtility.DeserializeUnityObject(this, ref serializationData);
+        }
+
+        [SerializeField, HideInInspector]
+        private SerializationData serializationData;
+        public SerializationData SerializationData 
+        { 
+            get => serializationData;
+            set => serializationData = value;
+        }
+        #endregion
+    }
+}
---
+++ Assets/MadDuck/Scripts/UIs/Notifications/NotificationView.cs
@@ -0,0 +1,76 @@
+﻿using Cysharp.Threading.Tasks;
+using MadDuck.Scripts.Utils;
+using PrimeTween;
+using Sirenix.OdinInspector;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace MadDuck.Scripts.UIs.Notifications
+{
+    public class NotificationView : MonoBehaviour
+    {
+        [Title("Inspectors")]
+        [SerializeField] private Image icon;
+        [SerializeField] private TMP_Text messageText;
+        
+        [Title("Settings")]
+        [SerializeField] private Sprite defaultIcon;
+
+        [Title("Tween")] 
+        [SerializeField] private TweenSettings<Vector2> showingRelativePositionTweenSettings;
+        [SerializeField] private ShakeSettings scaleTweenSettings;
+
+        private TweenSettings<Vector2> _relativePositionSettings;
+        private Sequence _visibilitySequence;
+        private Sequence _animationSequence;
+        
+        public void Initialize()
+        {
+            messageText.text = string.Empty;
+            icon.sprite = null;
+            _relativePositionSettings = showingRelativePositionTweenSettings.ToRelative(((RectTransform)transform).anchoredPosition);
+        }
+        
+        public async UniTask Show()
+        {
+            _visibilitySequence = Sequence.Create()
+                .Group(Tween.UIAnchoredPosition((RectTransform)transform, _relativePositionSettings));
+            await _visibilitySequence.ToUniTask();
+        }
+
+        public async UniTask PlayAnimation()
+        {
+            _animationSequence = Sequence.Create()
+                .Group(Tween.PunchScale(transform, scaleTweenSettings));
+            await _animationSequence.ToUniTask();
+        }
+
+        public async UniTask Hide()
+        {
+            _visibilitySequence = Sequence.Create()
+                .Group(Tween.UIAnchoredPosition((RectTransform)transform, _relativePositionSettings.WithDirection(false)));
+            await _visibilitySequence.ToUniTask();
+        }
+
+        public void Cancel()
+        {
+            _visibilitySequence.Complete();
+            _animationSequence.Complete();
+        }
+
+        public void SetMessage(string message)
+        {
+            messageText.text = message;
+        }
+
+        public void SetIcon(Sprite sprite)
+        {
+            if (!sprite)
+            {
+                sprite = defaultIcon;
+            }
+            icon.sprite = sprite;
+        }
+    }
+}
